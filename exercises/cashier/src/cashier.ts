cc77aa636630c4ec69f3279db38650bc511e7693
Lewis Miller <lemiller@linkedin.com>
2017-10-27 15:30:23 -0700

    completed the cashier tasks

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -1,3 +1,28 @@
+interface CartItem {
+  name: string,
+  price: number
+  qty?: number
+}
+
+class Cashier {
+  total: number = 0
+  length: number = 0
+
+  add(name: string, price: number, qty: number = 1): Cashier {
+    this.total += price * qty;
+    this.length += qty;
+
+    return this;
+  }
+
+  addItem({ name, price, qty = 1 }: CartItem): Cashier {
+    this.total += price * qty;
+    this.length += qty;
+
+    return this;
+  }
+}
+
 export function cashier() {
-  return {};
+  return new Cashier();
 }
