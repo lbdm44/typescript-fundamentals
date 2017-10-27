446a54f43bbffc387449273d7b41e63c247a5aaf
Lewis Miller <lemiller@linkedin.com>
2017-10-27 15:16:29 -0700

    created dealer implementation

--- exercises/dealer/src/dealer.ts
+++ exercises/dealer/src/dealer.ts
@@ -1,3 +1,18 @@
+export enum CardNumber {
+  Ace, Two, Three, Four, Five,
+  Six, Seven, Eight, Nine, Ten,
+  Jack, Queen, King
+}
+
+export enum Suit {
+  Clubs,
+  Diamons,
+  Hearts,
+  Spades
+}
+
+type Card = [Suit, CardNumber];
+
 /**
  * Shuffle an array in place
  * @param a Array to shuffle
@@ -12,6 +27,40 @@ function shuffleArray(a: any[]) {
   }
 }
 
-export class Dealer {
+function generateDeck(): Card[] {
+  let cards: Card[] = [];
 
+  for (let s = 0; s < Object.keys(Suit).length / 2; s++) {
+    for (let c = 0; c < Object.keys(CardNumber).length / 2; c++) {
+      cards.push([s, c]);
+    }
+  }
+
+  return cards;
+}
+
+export class Dealer {
+  cards: Card[]
+
+  constructor() {
+    this.cards = generateDeck();
+    shuffleArray(this.cards);
+  }
+
+  dealHand(handSize: number): Card[] {
+    if (handSize < 0) throw new Error('Give me cards?');
+    if (handSize > this.cards.length) throw new Error('No more cards in the deck.');
+
+    return this.cards.splice(0, handSize);
+  }
+
+  getLength(): number {
+    return this.cards.length;
+  }
+
+  readCard(card: Card): string {
+    let [cardSuit, cardNumber ] = card;
+
+    return `${CardNumber[cardNumber]} of ${Suit[cardSuit]}`;
+  }
 }
