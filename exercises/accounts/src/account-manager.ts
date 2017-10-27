913cfa1cc2892eb078bf01d1ef796f9434a1f02b
Lewis Miller <lemiller@linkedin.com>
2017-10-27 11:59:36 -0700

    fixing account-manager and color-utils

--- exercises/accounts/src/account-manager.ts
+++ exercises/accounts/src/account-manager.ts
@@ -1,5 +1,17 @@
+export interface IUser {
+  email: string,
+  password: string,
+}
+
+export interface IConfirmedUser extends IUser {
+  isActive: true,
+}
+export interface IAdmin extends IConfirmedUser {
+  adminSince: Date,
+}
+
 export class AccountManager {
-  users = new Array();
+  users: IUser[] = new Array();
 
   /**
    * Create a new user account
@@ -8,7 +20,7 @@ export class AccountManager {
    * @return the new user account. An admin must activate it using activateNewUser
    * @see this.activateNewUser
    */
-  register(email, password) {
+  register(email: string, password: string): IUser {
     if(!email) throw 'Must provide an email';
     if(!password) throw 'Must provide a password';
     let user = { email, password };
@@ -22,10 +34,9 @@ export class AccountManager {
    * @param userToApprove Newly-registered user, who is to be activated
    * @return the updated user object, now activated
    */
-  activateNewUser(approver, userToApprove) {
+  activateNewUser(approver: IAdmin, userToApprove: IUser): IConfirmedUser {
     if (!approver.adminSince) throw "Approver is not an admin!";
-    userToApprove.isActive = true;
-    return userToApprove;
+    return { ...userToApprove, isActive: true };
   }
 
   /**
@@ -34,10 +45,9 @@ export class AccountManager {
    * @param user an active user who you're making an admin
    * @return the updated user object, now can also be regarded as an admin
    */
-  promoteToAdmin(existingAdmin, user) {
+  promoteToAdmin(existingAdmin: IAdmin, user: IConfirmedUser): IAdmin {
     if (!existingAdmin.adminSince) throw "Not an admin!";
     if (user.isActive !== true) throw "User must be active in order to be promoted to admin!";
-    user.adminSince = new Date();
-    return user;
+    return { ...user, adminSince: new Date() };
   }
 }
--- exercises/color-functions/src/color-utils.ts
+++ exercises/color-functions/src/color-utils.ts
@@ -1,4 +1,43 @@
-//TODO: Implement hexToRgb
+function pad(value: string) {
+  let res = value;
 
+  while ((res.length - 2) < 0) {
+    res = `${res}0`;
+  }
 
-//TODO: Implement rgbToHex
+  return res;
+}
+
+interface RgbColor {
+  r: number,
+  g: number,
+  b: number,
+}
+
+export function hexToRgb(hex: string): RgbColor {
+  if (hex.length === 3) {
+    let hr = hex[0];
+    let hg = hex[1];
+    let hb = hex[2];
+
+    return hexToRgb(`${hr}${hr}${hg}${hg}${hb}${hb}`)
+  }
+
+  let red = hex.slice(0, 2);
+  let green = hex.slice(2, 4);
+  let blue = hex.slice(4);
+
+  return {
+    r: parseInt(red, 16),
+    g: parseInt(green, 16),
+    b: parseInt(blue, 16),
+  };
+}
+
+export function rgbToHex(r: number, g: number, b: number): string {
+  return [r, g, b]
+    .map(val => Math.max(Math.min(val, 255), 0))
+    .map(val => val.toString(16))
+    .map(pad)
+    .join('');
+}
