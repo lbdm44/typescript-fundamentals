53b57ece0f9e8fc00c5e90098c9e62eef972b489
Lewis Miller <lemiller@linkedin.com>
2017-10-30 10:22:32 -0700

    completed stack excersize

--- exercises/stack/src/stack.ts
+++ exercises/stack/src/stack.ts
@@ -6,6 +6,28 @@ interface IStack<T> {
   print(): void;
 }
 
-export class Stack<T> {
+export class Stack<T> implements IStack<T> {
+  private items: T[] = []
 
+  push(item: T): IStack<T>
+  push(items: T[]): IStack<T>
+  push(items: T | T[]): IStack<T> {
+    if (items instanceof Array) {
+      this.items.push(...items);
+    } else {
+      this.items.push(items);
+    }
+
+    return this;
+  }
+
+  pop(): T | undefined {
+    return this.items.pop();
+  }
+
+  length(): number {
+    return this.items.length;
+  }
+
+  print() {}
 }
